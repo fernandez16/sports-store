b6fd813a2c98cde4fec51b3babb0aeae3fe411dc
Raul <raulfernan000@gmail.com>
2022-06-02 09:20:15 +0200

    added pagination

--- src/app/store/store.component.ts
+++ src/app/store/store.component.ts
@@ -10,9 +10,15 @@ export class StoreComponent {
   constructor(private repository: ProductRepository) {}
 
   public selectedCategory = null;
+  public productsPerPage = 4;
+  public selectedPage = 1;
 
   get products(): Product[] {
-    return this.repository.getProducts(this.selectedCategory);
+    // return this.repository.getProducts(this.selectedCategory);
+    let pageIndex = (this.selectedPage - 1) * this.productsPerPage;
+    return this.repository
+      .getProducts(this.selectedCategory)
+      .slice(pageIndex, pageIndex + this.productsPerPage);
   }
 
   get categories(): string[] {
@@ -21,5 +27,25 @@ export class StoreComponent {
 
   changeCategory(newCategory?: string) {
     this.selectedCategory = newCategory;
+  }
+
+  changePage(newPage: number) {
+    this.selectedPage = newPage;
+  }
+
+  changePageSize(newSize: number) {
+    this.productsPerPage = Number(newSize);
+    this.changePage(1);
+  }
+
+  get pageNumbers(): number[] {
+    return Array(
+      Math.ceil(
+        this.repository.getProducts(this.selectedCategory).length /
+          this.productsPerPage
+      )
+    )
+      .fill(0)
+      .map((x, i) => i + 1);
   }
 }
